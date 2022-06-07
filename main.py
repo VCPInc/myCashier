f282012be1c2e3206bc5afc91309f28d06eb4596
Leo_A <50373651+AlphaL64@users.noreply.github.com>
2022-06-07 08:34:37 -0700

    changed my repl to my github

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def openreplit(name):
 			"Opened Noor's replit")
 
 	elif name == "AL":
-		webbrowser.open("https://github.com/AlphaL64"), print(
+		webbrowser.open("https://github.com/Leo_A"), print(
 			"Opened Antonios's thing")
 
 	elif name == "SW":
